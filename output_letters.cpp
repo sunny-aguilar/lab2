9a3ff8495d5d98d484a28df1c4e1ed7b2d544bfb
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-12-19 19:55:50 -0800

    functions

--- output_letters.cpp
+++ output_letters.cpp
@@ -4,21 +4,5 @@
 ** Description:     Write a short description
 **                  of your code
 **                  here.
-**
 *********************************************************************/
-#include <iostream>
-#include <fstream>
-using std::cin;
-using std::cout;
-using std::ifstream;
-using std::ofstream;
-using std::endl;
 
-
-
-int main() {
-    ofstream inputFile;
-    inputFile.open("file_fun.txt");
-
-    return 0;
-}
