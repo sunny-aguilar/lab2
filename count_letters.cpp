5b9061c634bdb422dc24a8ed068a1553d554ae22
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-12-28 13:44:35 -0800

    lab2

--- count_letters.cpp
+++ count_letters.cpp
@@ -8,14 +8,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <cctype>
 using std::cout;
 using std::ifstream;
 using std::string;
 using std::endl;
+using std::stringstream;
 
 void count_letters(ifstream &inFile, int*) {
+    stringstream ss;
     char alphabet[] = {"abcdefghijklmnopqrstuvwxyz"};
+    char charsInfile[27];
     char letter;
     char letterArr[100];
     char stringRead;
@@ -27,7 +31,20 @@ void count_letters(ifstream &inFile, int*) {
         cout << letter << endl;
         counter++;
     }
-    cout << letterArr[0] << endl;
-    cout << letterArr[1] << endl;
-    cout << letterArr[2] << endl;
+    cout << "Letters saved in array\n";
+    for (int i = 0; i < 52; i++) {
+        cout << letterArr[i] << " ";
+    }
+    cout << endl;
+
+    // 1. transfer file contents into a string stream;
+    // 2. convert char to lower case
+    // 3. all all of the characters in the input file are then
+    //    added to a new array
+    // 4. count frequency (2 approaches)
+    // 4a. count total instances of characters in the array
+    // 4n. only add characters to the array if they have not
+    //     been previously added.
+    // 5. output results to file
+
 }
