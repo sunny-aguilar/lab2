8f0b7a2a4fcd484f02ef7e752dd2f63cb3505b38
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-12-28 21:59:13 -0800

    lab2

--- count_letters.cpp
+++ count_letters.cpp
@@ -1,9 +1,14 @@
 /*********************************************************************
 ** Author:          Sandro Aguilar
 ** Date:            Jan 2019
-** Description:     Write a short description
-**                  of your code
-**                  here.
+** Description:     cout_letters function accepts a reference to a file
+**                  object and a pointer to an int array. The function
+**                  reads in a file specified by user. For each
+**                  paragraph, the function counts the frequency of
+**                  each letter and ask the user what filename to
+**                  output to. Function output_letters is then called
+*                  with output file and pointer to an in array
+ *                  parameters.
 *********************************************************************/
 #include "output_letters.hpp"
 #include <iostream>
